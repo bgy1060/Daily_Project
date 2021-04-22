213af574fcc46496e9baefc9426d8539bfd04ec2
bgy1060 <40761315+bgy1060@users.noreply.github.com>
2021-04-22 10:58:54 +0900

    add lendit balance

--- lendit/views.py
+++ lendit/views.py
@@ -129,3 +129,103 @@ class LenditViewSet(viewsets.GenericViewSet):
         serializer = CompanyAccountSerializer(query_set)
         return JsonResponse(serializer.data, safe=False)
         return Response(status=status.HTTP_201_CREATED)
+
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'company_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='투자 요약 정보를 가져오고 싶은 회사 ID'),
+
+        }))
+    @csrf_exempt
+    @action(methods=['POST', ], detail=False, permission_classes=[IsAuthenticated, ])
+    def balance(self, request):
+        """ USER 투자 요약 정보 가져오기 [token required] """
+        company_id = Company.objects.get(id=int(request.data['company_id']))
+
+        # 세션 시작하기
+        session = requests.session()
+
+        try:
+            with open('C:/Users/daily-funding/Desktop/cookie/' + str(request.user.id) + '_' + str(
+                    company_id.id) + '_cookie.txt', 'rb') as f:
+                session.cookies.update(pickle.load(f))
+
+        except:
+
+            username = request.user.register_set.get(company_id=company_id).username.strip()
+            password = request.user.register_set.get(company_id=company_id).user_password.strip()
+
+            decrypted_username = AES.AESCipher(bytes(my_settings.key)).decrypt(username)
+            decrypted_password = AES.AESCipher(bytes(my_settings.key)).decrypt(password)
+
+            USER = decrypted_username
+            PASS = decrypted_password
+
+            login_info = {
+                "email": USER,  # 아이디 지정
+                "password": PASS,  # 비밀번호 지정
+            }
+
+            url_login = "https://invest.lendit.co.kr/signin"
+            res = session.post(url_login, data=login_info)
+            res.raise_for_status()  # 오류가 발생하면 예외가 발생합니다.
+
+            with open('C:/Users/daily-funding/Desktop/cookie/' + str(request.user.id) + '_' + str(
+                    company_id.id) + '_cookie.txt', 'wb') as f:
+                pickle.dump(session.cookies, f)
+
+        # 마이페이지에 접근하기
+        url_mypage = "https://invest.lendit.co.kr/api/mypage/portfolios/summary"
+        res = session.get(url_mypage)
+        res.raise_for_status()
+
+        try:
+            data = res.json()
+        except:
+            username = request.user.register_set.get(company_id=company_id).username.strip()
+            password = request.user.register_set.get(company_id=company_id).user_password.strip()
+
+            decrypted_username = AES.AESCipher(bytes(my_settings.key)).decrypt(username)
+            decrypted_password = AES.AESCipher(bytes(my_settings.key)).decrypt(password)
+
+            USER = decrypted_username
+            PASS = decrypted_password
+
+            login_info = {
+                "email": USER,  # 아이디 지정
+                "password": PASS,  # 비밀번호 지정
+            }
+
+            url_login = "https://invest.lendit.co.kr/signin"
+            res = session.post(url_login, data=login_info)
+            res.raise_for_status()  # 오류가 발생하면 예외가 발생합니다.
+
+            with open('C:/Users/daily-funding/Desktop/cookie/' + str(request.user.id) + '_' + str(
+                    company_id.id) + '_cookie.txt', 'wb') as f:
+                pickle.dump(session.cookies, f)
+
+            # 마이페이지에 접근하기
+            url_mypage = "https://invest.lendit.co.kr/api/mypage/portfolios/summary"
+            res = requests.get(url_mypage, cookies=session.cookies)
+            res.raise_for_status()
+            data = res.json()
+
+        total_investment = int(data['result']['accumPrincipal'])
+        number_of_investing_products = data['result']['totalNoteCnt']
+        residual_investment_price = data['result']['netRemainPrincipal']
+
+        try:
+            request.user.investing_balance_set.create(total_investment=total_investment,
+                                                      number_of_investing_products=number_of_investing_products,
+                                                      residual_investment_price=residual_investment_price,
+                                                      company_id=company_id, uid=request.user.id)
+        except:
+            request.user.investing_balance_set.filter(uid=request.user.id, company_id=company_id).update(
+                total_investment=total_investment,
+                number_of_investing_products=number_of_investing_products,
+                residual_investment_price=residual_investment_price)
+
+        query_set = request.user.investing_balance_set.get(company_id=company_id)
+        serializer = CompanyBalanceSerializer(query_set)
+        return JsonResponse(serializer.data, safe=False)
+        return Response(status=status.HTTP_201_CREATED)
